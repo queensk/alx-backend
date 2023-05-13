135b906cf0a0889206dad7ca7734a4f06b44fe7d
queensk <queenskisivuli@gmail.com>
2023-05-13 22:38:22 +0300

    pycodestyle fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,15 +37,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
-            paginate dataset
-            """
-            assert type(page) is int and page > 0
-            assert type(page_size) is int and page_size > 0
+        """
+        paginate dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            index = index_range(page, page_size)
-            try:
-                data = self.dataset()
-                return data[index[0]: index[1]]
-            except IndexError:
-                return []
+        index = index_range(page, page_size)
+        try:
+            data = self.dataset()
+            return data[index[0]: index[1]]
+        except IndexError:
+            return []
