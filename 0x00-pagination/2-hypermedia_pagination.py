1e4991b5bbbca8899babeaf3011127d880cf8e89
queensk <queenskisivuli@gmail.com>
2023-05-13 22:40:50 +0300

    fix pycodestyle

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -37,19 +37,19 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
-            paginate dataset
-            """
-            assert type(page) is int and page > 0
-            assert type(page_size) is int and page_size > 0
+        """
+        paginate dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            index = index_range(page, page_size)
-            try:
-                data = self.dataset()
-                return data[index[0]: index[1]]
-            except IndexError:
-                return []
-    
+        index = index_range(page, page_size)
+        try:
+            data = self.dataset()
+            return data[index[0]: index[1]]
+        except IndexError:
+            return []
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Hypermedia pagination
@@ -60,10 +60,10 @@ class Server:
         prev_page = page - 1 if page > 1 else None
 
         return {
-                "page_size": len(data),
-                "page": page,
-                "data": data,
-                "next_page": next_page,
-                "prev_page": prev_page,
-                "total_pages": total_pages
-                }
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
