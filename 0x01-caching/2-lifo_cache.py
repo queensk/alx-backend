2d6309750b219902f3ac85e95264a493f516991b
queensk <queenskisivuli@gmail.com>
2023-05-20 05:38:15 +0300

    fifo cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+LIFO Caching
+"""
+BaseCaching = __import__('base_cache').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFO Caching
+    """
+
+    def __init__(self):
+        """
+        initialize
+        """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS\
+                    and key not in self.stack:
+                del_key = self.stack.pop()
+                del self.cache_data[del_key]
+                print("DISCARD: {}".format(del_key))
+            self.cache_data[key] = item
+            self.stack.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        value = self.cache_data.get(key)
+        return value
