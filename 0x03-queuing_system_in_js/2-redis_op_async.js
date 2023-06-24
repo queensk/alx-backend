56abf4f000501c51f9367be3cd1d87725779733d
queensk <queenskisivuli@gmail.com>
2023-06-24 08:26:16 +0300

    npm run dev 2-redis_op_async.js

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setNewSchool = async (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = async (schoolName) => {
+  const getAsync = promisify(client.get).bind(client);
+  try {
+    const reply = await getAsync(schoolName);
+    console.log(reply);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
